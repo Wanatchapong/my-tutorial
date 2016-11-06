3a3b7c0d603707e03d9db58f43d3532830faf73a
wanatchapong <bewbangluang@gmail.com>
2016-11-06 14:14:43 +0700

    test count and exclude duplicate in list

--- src/test/java/com/me/tutorial/ListTest.java
+++ src/test/java/com/me/tutorial/ListTest.java
@@ -2,9 +2,7 @@ package com.me.tutorial;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by wanatchapong
@@ -20,5 +18,37 @@ public class ListTest {
         list.addAll(existList);
 
         list.forEach(System.out::println);
+    }
+
+    @Test
+    public void countDuplicate() throws Exception {
+        List<String> duplicatedList = new LinkedList<>();
+
+        for (int i = 0; i < 10; i++) {
+            duplicatedList.add(String.valueOf(i));
+        }
+        for (int i = 0; i < 5; i++) {
+            duplicatedList.add(String.valueOf(i));
+        }
+
+        System.out.println("My List : " + duplicatedList);
+
+        final Set<String> duplicateSet = new HashSet<>();
+        final Set<String> uniqueSet = new HashSet<>();
+
+        for (String element : duplicatedList) {
+
+            //If this set already contains the element, the call leaves the set unchanged and returns false.
+            if (!uniqueSet.add(element)) {
+                duplicateSet.add(element);
+            }
+        }
+
+        System.out.println("\nHere are the unique elements from list : " + uniqueSet);
+        System.out.println("\nHere are the duplicate elements from list : " + duplicateSet);
+
+        for (String s : uniqueSet) {
+            System.out.println("Frequency of " + s + " is " + Collections.frequency(duplicatedList, s));
+        }
     }
 }
